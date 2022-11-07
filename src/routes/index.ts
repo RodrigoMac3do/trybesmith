9ff575345a23e0adaf47badb735a25fd07ee187b
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-07 19:29:22 -0300

    feat: import e export de routes

---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+import productsRoute from './products.route';
+import usersRoute from './users.route';
+import ordersRoute from './orders.route';
+import loginRoute from './login.route';
+
+export { productsRoute, usersRoute, ordersRoute, loginRoute };
