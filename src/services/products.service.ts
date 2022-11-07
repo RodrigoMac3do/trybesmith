8b1a669c1929ec2dbf294b33d865388599cc7b7f
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-07 19:58:17 -0300

    feat: getAll traz todos os produtos do DB

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -4,6 +4,11 @@ import ProductsModel from '../models';
 export default class ProductsService {
   public products = new ProductsModel();
 
+  public async getAll(): Promise<IProductsID[]> {
+    const products = await this.products.getAll();
+    return products;
+  }
+
   public create(products: IProducts): Promise<IProductsID> {
     return this.products.create(products);
   }
