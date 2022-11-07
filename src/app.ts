eef751aed3c30729f7c39f6c95390fd0437d6e39
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-07 19:18:46 -0300

    feat: criação de rotas + middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import { productsRoute, usersRoute, ordersRoute, loginRoute } from './routes/index';
+import httpErrorMiddleware from './middlewares/http.error.middleware';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productsRoute);
+
+app.use('/users', usersRoute);
+
+app.use('/orders', ordersRoute);
+
+app.use('/login', loginRoute);
+
+app.use(httpErrorMiddleware);
 
 export default app;
