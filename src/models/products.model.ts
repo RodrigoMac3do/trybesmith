599540e6f6d92161b26e4f736b82bc5517936de8
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-07 19:59:01 -0300

    feat: getAll traz todos os produtos do DB

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,10 +1,20 @@
-import { ResultSetHeader } from 'mysql2/promise';
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 
 import { IProductsID, IProducts } from '../interfaces';
 import mysql from './db/connection';
 
 export default class ProductsModel {
   private connection = mysql;
+
+  public async getAll(): Promise<IProductsID[]> {
+    const query = 'SELECT * FROM Trybesmith.Products';
+
+    const [rows] = await this.connection.execute<
+    IProductsID[] & RowDataPacket[]
+    >(query);
+
+    return rows;
+  }
 
   public async create(products: IProducts): Promise<IProductsID> {
     const { name, amount } = products;
