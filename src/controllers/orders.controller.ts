21cf84d2a73589ab748e78d76217ac11017f28a1
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-10 16:51:51 -0300

    feat: controller para orders

---
+++ src/controllers/orders.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import { OrdersService } from '../services';
+
+export default class OrdersController {
+  public ordersService = new OrdersService();
+
+  async getAll(_req: Request, res: Response) {
+    const orders = await this.ordersService.getAll();
+
+    res.status(200).json(orders);
+  }
+}
