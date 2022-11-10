251697f92dec6024e0f28b0faa2e365ba524a10a
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-10 16:35:18 -0300

    style: nome de variavel user-> users

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -5,10 +5,10 @@ export default class UsersController {
   public usersService = new UsersService();
 
   async create(req: Request, res: Response) {
-    const user = req.body;
+    const users = req.body;
 
-    await this.usersService.create(user);
-    
+    await this.usersService.create(users);
+
     res.status(201).json({
       token: 'eyJhbGciOiJISflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
     });
