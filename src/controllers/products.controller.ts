6f27d0efee1ae6d7927bc3964593513ac472df13
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-10 16:22:01 -0300

    chore: altera import e style do create

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import ProductsService from '../services';
+import { ProductsService } from '../services';
 
 export default class ProductsController {
   public productsService = new ProductsService();
@@ -12,8 +12,8 @@ export default class ProductsController {
 
   async create(req: Request, res: Response) {
     const products = req.body;
-
     const productsCreated = await this.productsService.create(products);
+    
     res.status(201).json(productsCreated);
   }
 }
