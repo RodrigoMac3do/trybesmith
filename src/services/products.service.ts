3308f645772d8e618fbd8118871f6190f5fe8329
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-10 16:31:05 -0300

    chore: alteção de import e style

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,15 +1,18 @@
 import { IProductsID, IProducts } from '../interfaces';
-import ProductsModel from '../models';
+import { ProductsModel } from '../models';
 
 export default class ProductsService {
   public products = new ProductsModel();
 
   public async getAll(): Promise<IProductsID[]> {
     const products = await this.products.getAll();
+    
     return products;
   }
 
-  public create(products: IProducts): Promise<IProductsID> {
-    return this.products.create(products);
+  public async create(products: IProducts): Promise<IProductsID> {
+    const product = await this.products.create(products);
+
+    return product;
   }
 }
