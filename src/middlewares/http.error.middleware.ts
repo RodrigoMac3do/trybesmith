18675e38ea16ea693c9b8b16eec6b3c35b462b49
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-11 22:36:26 -0300

    refactor:  tipificação de middleware de erro

--- src/middlewares/http.error.middleware.ts
+++ src/middlewares/http.error.middleware.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
 import HttpException from '../utils/http.exception';
 
-const httpErrorMiddleware = (err: Error, req: Request, res: Response) => {
+const httpErrorMiddleware: ErrorRequestHandler = (err, req, res) => {
   const { status, message } = err as HttpException;
-  res.status(status || 500).json({ message });
+
+  return res.status(status || 500).json({ message });
 };
 
 export default httpErrorMiddleware;
