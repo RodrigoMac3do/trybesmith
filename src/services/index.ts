c4a68dd26799878fdd58fc4cd6b380d4ebc243fd
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-11 22:41:43 -0300

    add: import e export de login service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 import ProductsService from './products.service';
 import UsersService from './users.service';
 import OrdersService from './orders.service';
+import LoginService from './login.service';
 
-export { ProductsService, UsersService, OrdersService };
+export { ProductsService, UsersService, OrdersService, LoginService };
