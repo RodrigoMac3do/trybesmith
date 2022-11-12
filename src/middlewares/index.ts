5024a4d5f98519ff39864b3f7766a468841dbe53
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-11 22:34:31 -0300

    feat: import e export de middlewares

---
+++ src/middlewares/index.ts
@@ -0,0 +1,5 @@
+import username from './username.middleware';
+import password from './password.middleware';
+import httpErrorMiddleware from './http.error.middleware';
+
+export { username, password, httpErrorMiddleware };
