125750f1aba72021bb6692d2fc888404245c2b9d
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-11 22:42:57 -0300

    add: import e export de inteface login

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,6 @@
 import { IProducts, IProductsID } from './IProducts';
 import { IUsers, IUsersID } from './IUsers';
 import { IOrders, IOrdersID } from './IOrders';
+import { ILogin } from './ILogin';
 
-export { IProducts, IProductsID, IUsers, IUsersID, IOrders, IOrdersID };
+export { IProducts, IProductsID, IUsers, IUsersID, IOrders, IOrdersID, ILogin };
