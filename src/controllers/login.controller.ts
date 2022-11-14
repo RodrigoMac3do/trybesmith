d99716d4f0033f7b52c5ce9040e0db5b759a23cb
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-14 18:44:18 -0300

    refactor: remove try catch

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,18 +1,15 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
+import { ILogin } from '../interfaces';
 import LoginService from '../services/login.service';
 
 export default class LoginController {
   public service = new LoginService();
 
-  async verify(req: Request, res: Response, next: NextFunction) {
+  async verify(req: Request<object, object, ILogin>, res: Response) {
     const { body } = req;
 
-    try {
-      const data = await this.service.verify(body);
+    const token = await this.service.verify(body);
 
-      res.status(200).json(data);
-    } catch (error) {
-      next(error);
-    }
+    return res.status(200).json({ token });
   }
 }
