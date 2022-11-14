3013f1eeb35cdec0035532b8e6fb3ac1cfc2f896
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-14 18:59:59 -0300

    refactor: jwt de forma dinamica

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -1,11 +1,10 @@
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
-import { ILogin } from '../interfaces';
 
 dotenv.config();
 
-const createToken = (data: ILogin) => {
-  const payload = { username: data.username, password: data.password };
+const createToken = (data: object) => {
+  const payload = { data };
 
   const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
     expiresIn: '8h',
