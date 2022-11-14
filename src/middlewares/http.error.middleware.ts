c50c348785ed03ed5386b649bcb33b802dfd53db
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-14 18:43:04 -0300

    add: next no final da função

--- src/middlewares/http.error.middleware.ts
+++ src/middlewares/http.error.middleware.ts
@@ -1,10 +1,12 @@
 import { ErrorRequestHandler } from 'express';
 import HttpException from '../utils/http.exception';
 
-const httpErrorMiddleware: ErrorRequestHandler = (err, req, res) => {
+const httpErrorMiddleware: ErrorRequestHandler = (err, _req, res, next) => {
   const { status, message } = err as HttpException;
 
-  return res.status(status || 500).json({ message });
+  res.status(status || 500).json({ message });
+
+  next();
 };
 
 export default httpErrorMiddleware;
