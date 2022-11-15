18c519b70eb18018b80e284361043575d3d8d652
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-15 11:22:27 -0300

    add: uso de joi para validar campos

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from 'express';
 import { UsersService } from '../services';
+import { userSchema } from '../services/validations/schema';
+import validateSchema from '../services/validations/validationSchema';
 
 export default class UsersController {
   public usersService = new UsersService();
 
   async create(req: Request, res: Response) {
-    const users = req.body;
+    const { body } = req;
 
-    const token = await this.usersService.create(users);
+    await validateSchema(userSchema, body);
+
+    const token = await this.usersService.create(body);
 
     res.status(201).json({ token });
   }
