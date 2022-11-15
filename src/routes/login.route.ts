6dbb10ca961ac1010bbcc4574945c69a3873dd36
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-15 08:18:11 -0300

    refactor: remove middleware e para uso do Joi

--- src/routes/login.route.ts
+++ src/routes/login.route.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import { LoginController } from '../controllers';
-import { username, password } from '../middlewares';
 
 const router = Router();
 
 const controller = new LoginController();
 
-router.post('/', username, password, controller.verify.bind(controller));
+router.post('/', controller.verify.bind(controller));
 
 export default router;
