8ec596199bf1b14ec10a2d323407aa72e0b974d6
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-15 11:21:26 -0300

    add: uso de joi para validar campos

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 import { ILogin } from '../interfaces';
+import { loginSchema } from '../services/validations/schema';
 import LoginService from '../services/login.service';
+import validateSchema from '../services/validations/validationSchema';
 
 export default class LoginController {
   public service = new LoginService();
 
   async verify(req: Request<object, object, ILogin>, res: Response) {
     const { body } = req;
+
+    await validateSchema(loginSchema, body);
 
     const token = await this.service.verify(body);
 
