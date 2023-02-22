ecff87f4f0816d05471cde8fabbda6e336b32346
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-22 00:25:19 -0300

    feat: class OrdersService

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,12 +1,31 @@
 import { IOrders } from '../interfaces';
-import { OrdersModel } from '../models';
+import { OrdersModel, ProductsModel } from '../models';
 
 export default class OrdersService {
-  public model = new OrdersModel();
+  private model: OrdersModel;
 
-  public async getAll(): Promise<IOrders[]> {
-    const orders = await this.model.getAll();
+  private modelProducts: ProductsModel;
+
+  constructor() {
+    this.model = new OrdersModel();
+    this.modelProducts = new ProductsModel();
+  }
+
+  public findAll = async (): Promise<IOrders[]> => {
+    const orders = await this.model.findAll();
 
     return orders;
-  }
+  };
+
+  public create = async (userId: number, body: IOrders): Promise<IOrders> => {
+    const { productsIds } = body;
+
+    const newOrder = await this.model.create(userId);
+
+    await Promise.all(
+      productsIds.map((id) => this.modelProducts.update(newOrder, id)),
+    );
+
+    return { userId, productsIds };
+  };
 }
