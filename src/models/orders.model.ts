41d28c743164a3125c0c57e9ff339221e837d4cc
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-22 00:31:12 -0300

    feat: class OrdersModel

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -1,27 +1,43 @@
-import { RowDataPacket } from 'mysql2/promise';
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { IOrders } from '../interfaces';
 import mysql from './connection';
 
 export default class OrdersModel {
   private connection = mysql;
 
-  public async getAll(): Promise<IOrders[]> {
+  public findAll = async (): Promise<IOrders[]> => {
     const query = `
-  SELECT 
-    O.id AS id,
-    O.userId AS userId,
+    SELECT 
+      O.id AS id,
+      O.userId AS userId,
       JSON_ARRAYAGG(P.id) AS productsIds
-  FROM 
-    Trybesmith.Orders AS O
-  INNER JOIN 
-    Trybesmith.Products AS P 
-      ON P.orderId = O.id
-  GROUP BY id`;
+    FROM 
+      Trybesmith.Orders AS O
+    INNER JOIN 
+      Trybesmith.Products AS P 
+        ON P.orderId = O.id
+    GROUP BY 
+      id`;
 
     const [rows] = await this.connection.execute<IOrders[] & RowDataPacket[]>(
       query,
     );
 
     return rows;
-  }
+  };
+
+  public create = async (userId: number): Promise<number> => {
+    const query = `
+    INSERT INTO Trybesmith.Orders
+      (userId )
+    VALUES 
+      (?)`;
+
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      query,
+      [userId],
+    );
+
+    return insertId;
+  };
 }
