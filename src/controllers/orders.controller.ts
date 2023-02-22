bb335aae2bfe0590f70eb2580dc16599e7ee86b8
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-22 00:35:25 -0300

    feat: class OrdersController

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,12 +1,45 @@
-import { Request, Response } from 'express';
+import { RequestHandler } from 'express';
 import { OrdersService } from '../services';
+import { ordersSchema } from '../services/validations/Schema';
+import ValidateSchema from '../services/validations/ValidationSchema';
+import HttpException from '../utils/HttpException';
+import Jwt from '../utils/jwt.util';
 
 export default class OrdersController {
-  public service = new OrdersService();
+  private service: OrdersService;
 
-  async getAll(_req: Request, res: Response) {
-    const orders = await this.service.getAll();
+  private jwt: Jwt;
+
+  private validateSchema: ValidateSchema;
+
+  constructor() {
+    this.service = new OrdersService();
+    this.validateSchema = new ValidateSchema();
+    this.jwt = new Jwt();
+  }
+
+  public findAll: RequestHandler = async (_req, res) => {
+    const orders = await this.service.findAll();
 
     res.status(200).json(orders);
-  }
+  };
+
+  public create: RequestHandler = async (req, res) => {
+    const { body } = req;
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      throw new HttpException(401, 'Token not found');
+    }
+
+    this.jwt.verifyToken(authorization);
+
+    const { id } = this.jwt.decoderToken(authorization);
+
+    await this.validateSchema.validate(ordersSchema, body);
+
+    const order = await this.service.create(id, body);
+
+    res.status(201).json(order);
+  };
 }
