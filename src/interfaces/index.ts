fc42461b11c5f183eca1a3a0c06bdd79fbe1edb8
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-22 11:31:03 -0300

    add: import e export de interface IToken

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,5 +2,15 @@ import { IProducts, IProductsID } from './IProducts';
 import { IUsers, IUsersID } from './IUsers';
 import { IOrders, IOrdersID } from './IOrders';
 import { ILogin } from './ILogin';
+import { IToken } from './IToken';
 
-export { IProducts, IProductsID, IUsers, IUsersID, IOrders, IOrdersID, ILogin };
+export {
+  IProducts,
+  IProductsID,
+  IUsers,
+  IUsersID,
+  IOrders,
+  IOrdersID,
+  ILogin,
+  IToken,
+};
