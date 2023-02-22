c09b35f7a7ca9c6268471995e3351c78c0b6927d
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-22 00:27:23 -0300

    feat: clss Jwt

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -1,19 +1,34 @@
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+import HttpException from './HttpException';
 
 dotenv.config();
 
-export default class CreateToken {
-  public jwt = jwt;
+export default class Jwt {
+  private jwt = jwt;
 
-  public createToken(data: object): string {
-    const payload = { data };
-
+  public createToken = (payload: object): string => {
     const token = this.jwt.sign(payload, process.env.JWT_SECRET as string, {
       expiresIn: '8h',
       algorithm: 'HS256',
     });
 
     return token;
-  }
+  };
+
+  public verifyToken = (token: string): jwt.JwtPayload | string => {
+    try {
+      const payload = jwt.verify(token, process.env.JWT_SECRET as string);
+
+      return payload;
+    } catch (error) {
+      throw new HttpException(401, 'Invalid token');
+    }
+  };
+
+  public decoderToken = (token: string): jwt.JwtPayload => {
+    const payload = jwt.decode(token);
+
+    return payload as object;
+  };
 }
