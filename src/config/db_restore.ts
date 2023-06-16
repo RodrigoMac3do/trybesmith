6ebb9602a2bbf358b337628ec6fc8f9d444987d9
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-06-15 21:23:30 -0300

    fix: correção de lint e documentação adicionada

--- src/config/db_restore.ts
+++ src/config/db_restore.ts
@@ -1,27 +1,38 @@
+/* eslint-disable no-await-in-loop */
 import fs from 'fs';
 import path from 'path';
-
 import { Pool } from 'mysql2/promise';
 import connection from '../models/connection';
 
+/**
+ * Função responsável por recriar o banco de dados a partir de um arquivo SQL.
+ * @param conn Objeto de conexão do banco de dados.
+ */
 export default async function recreateDatabase(conn: Pool) {
   try {
     const importPath = path.resolve(__dirname, '../../Trybesmith.sql');
     const seedDBContent = fs.readFileSync(importPath).toString();
     const queries = seedDBContent.split(';').filter((p) => p.trim());
+
     for (let i = 0; i < queries.length; i += 1) {
       const query = queries[i];
       await conn.query(query);
     }
   } catch (error) {
-    console.log('Banco Falha em restaurar o Banco', error);
+    console.info('Falha ao recriar o banco de dados:', error);
   }
 }
 
+// Verifica se este módulo é o módulo principal
 if (require.main === module) {
-  recreateDatabase(connection).then(async () => {
-    console.log('Banco Restaurado com sucesso');
-    await connection.end();
-    process.exit(0);
-  });
+  recreateDatabase(connection)
+    .then(async () => {
+      console.info('Banco de dados restaurado com sucesso');
+      await connection.end(); // Encerra a conexão com o banco de dados
+      process.exit(0); // Finaliza o processo com código de saída 0 (sucesso)
+    })
+    .catch((error) => {
+      console.error('Erro ao restaurar o banco de dados:', error);
+      process.exit(1); // Finaliza o processo com código de saída 1 (erro)
+    });
 }
